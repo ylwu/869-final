decea58ca7ca4ee673316327eff6768d546ed276
Yonglin Wu <ylwu@Yonglins-MacBook-Pro.local>
2014-11-24 23:19:00 -0500

    some bug fixes

--- src/Find_window_vector.m
+++ src/Find_window_vector.m
@@ -1,7 +1,7 @@
 function v = Find_window_vector(p, image, m)
 
 %will not include colors outsdie the image
-[row, col]=size(image);
+[row, col, ~]=size(image);
 
     if mod(m,2)==0, %even
         left = floor(m/2)-1;
@@ -11,10 +11,10 @@ function v = Find_window_vector(p, image, m)
         right = floor(m/2);
     end
 
-    start_row = max(1,p(1)-left);
-    end_row  = min(row,p(1)+right);
-    start_col = max(1,p(2)-left);
-    end_col = min(col, p(2)+right);
+    start_row = max(1,floor(p(1))-left);
+    end_row  = min(row,floor(p(1))+right);
+    start_col = max(1,floor(p(2))-left);
+    end_col = min(col, floor(p(2))+right);
     v=zeros(m*m,3);
     count=1;
     for i=start_row:end_row,
--- src/main.m
+++ src/main.m
@@ -1,15 +1,19 @@
+clear;
 k = 4; % number of neighbors
 m = 5; %window size
-thresh = 6;
+thresh = .6;
+D_min = 0;
 D_max = 10; %TODO: change D_max to actual value
-d_inc = 0.1; %search increment for D
+d_inc = 0.3; %search increment for D
 
 height = 480;
 width  = 640;
 nImages = 47;
 all_depth_map = zeros(nImages,height,width);
 for imageIndex = 1:nImages
-   neighbor_indices = findNeighbors(imageIndex,k,31);
+    disp('imageIndex');
+    disp(imageIndex);
+   neighbor_indices = findNeighbors(imageIndex,k,nImages);
    im = readImage(imageIndex);
    im_h = size(im,1);
    im_w = size(im,2);
@@ -22,28 +26,43 @@ for imageIndex = 1:nImages
            v0 = Find_window_vector([rowIndex,colIndex],im,m);
            d_max = 0;
            CV_max = 0;
-           size = 0;
-           for d = 0: d_inc: D_max
+           s = 0;
+           for d = D_min: d_inc: D_max
                 CV = [];
-                for n = 1:neighbor_indices
+                for n = neighbor_indices
                     [K1,R1,t1] = findLocation(n);
                     PK = projectionMatrix(K1,R1,t1);
                     pk = Pixel_location([rowIndex,colIndex],d,P0,PK);
-                    im_k = readImage(n);
-                    v1 = Find_window_vector(pk,n,m);
-                    ncc = Ncc(v0,v1);
-                    if ncc > thresh
-                        CV = [CV ncc];
+                    if pk(1) <= 640 && pk(1) > 0 && pk(2) <= 480 && pk(2) > 0
+%                         rowIndex
+%                         colIndex
+%                         pk
+%                         d
+                        im_k = readImage(n);
+                        v1 = Find_window_vector(pk,im_k,m);
+                        ncc = Ncc(v0,v1);
+                        if ncc > thresh
+                            disp(ncc);
+                            CV = [CV ncc];
+                        end
                     end
                 end
                 CV_valid = Corr(CV);
                 if CV_valid > CV_max
                     CV_max = CV_valid;
+                    clear d_max;
                     d_max = d;
-                    size = size(CV,2);
+                    if (~isempty(CV))
+                        s = size(CV,2);
+                        disp('success');
+                        d
+                    end
                 end
            end
-           conf = Conf(CV_max,thresh,size);
+           rowIndex
+           colIndex
+           d_max
+           conf = Conf(CV_max,thresh,s);
            all_depth_map(imageIndex,rowIndex,colIndex) = conf;
        end
    end

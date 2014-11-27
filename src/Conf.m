51c6287446f5d956362f169f4e9175b8d83b7bcf
Yonglin Wu <ylwu@dhcp-18-111-55-138.dyn.mit.edu>
2014-11-26 21:31:45 -0500

    fixed main.m

--- src/Conf.m
+++ src/Conf.m
@@ -1,6 +1,6 @@
 function conf = Conf(Cv_max, thresh, v_size)
     if size(Cv_max,2)==0,
-        conf = 'NAN';
+        conf = NaN;
     else
         numerator = sum(Cv_max-thresh);
         denom = v_size*(1-thresh);
--- src/Corr.m
+++ src/Corr.m
@@ -1,9 +1,7 @@
 function corr = Corr(Cv)
     if size(Cv,2)==0,
-        corr = 'NAN';
+        corr = NaN;
     else
-        numerator = sum(Cv);
-        denom = size(Cv,2);
-        corr = numerator/denom;
+        corr = mean(Cv);
     end
 end

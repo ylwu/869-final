678b2076754b31e6fb05868981e461440f081956
Yonglin Wu <ylwu@dhcp-18-111-7-14.dyn.mit.edu>
2014-12-05 00:07:36 -0500

    modified pointCloud function

--- src/pointCloud.m
+++ src/pointCloud.m
@@ -1,32 +1,39 @@
-a=load('depth_map8_9.mat');
-depth8=a.all_depth_map(:,:,8);
+load('all_depth_map_1.mat');
+load('weight_map_1.mat');
 
+row = 480;
+col = 640;
+
+output=zeros(3,row*col*6);
 fileID = fopen('../data/templeRing/templeR_par.txt');
 C=textscan(fileID,'%s %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f');
 fclose(fileID);
-[K,R,t] = findLocation(8,C);
-P = projectionMatrix(K,R,t);
-invp=inv(P);
-
-[row,col]=size(depth8);
-output=zeros(row*col,3);
 count=1;
-for x=1:row,
-    for y=1:col,
-        z=depth8(x,y,:);
-        v=invp*[x;y;1;z];
-       % v=v/v(4);
-        output(count,:)=v(1:3);
-        count=count+1;
+
+for i=1:5:26
+    depth=all_depth_map(:,:,i);
+    [K,R,t] = findLocation(i,C);
+    P = projectionMatrix(K,R,t);
+    invp=inv(P);
+
+    for y=1:row
+        for x=1:col
+            z=depth(y,x,1);
+            if z~=0 && weight_map(y,x,i) >700
+                v=invp*[x;y;1;z];
+                v=v/v(4);
+                output(:,count)=v(1:3);
+                count=count+1;
+            end
+        end
     end
 end
-
-fid = fopen('depth8.txt', 'w');
+fid = fopen('depth.ply', 'w');
 
 % print a title, followed by a blank line
-fprintf(fid, 'ply\n format ascii 1.0\n element vertex 307200\n property float x\n property float y\n property float z\n end_header\n');
+fprintf(fid, 'ply\n format ascii 1.0\n element vertex %d\n property float x\n property float y\n property float z\n end_header\n',count);
 
 % print values in column order
 % two values appear on each row of the file
-fprintf(fid, '%f  %f %f\n', output);
+fprintf(fid, '%f  %f %f\n', output(:,1:count));
 fclose(fid);

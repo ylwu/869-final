2568267a323e3264e55a86578811768d8b0a5525
shiyangliu <lsyang@mit.edu>
2014-12-06 10:57:31 -0500

    added verify

---
+++ src/display_3.m
@@ -0,0 +1,6 @@
+im3=readImage(13);
+imshow(im3);
+%imtool(im3)
+hold on
+p=[511,240];
+plot(p(1),p(2),'r.','MarkerSize',20)
---
+++ src/verify_transform.m
@@ -0,0 +1,21 @@
+
+
+fileID = fopen('../data/templeRing/templeR_par.txt');
+C=textscan(fileID,'%s %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f');
+fclose(fileID);
+[K6,R6,t6] = findLocation(13,C);
+P6 = projectionMatrix(K6,R6,t6);
+[K3,R3,t3] = findLocation(45,C); 
+P3 = projectionMatrix(K3,R3,t3);
+
+p=[511,240];
+
+im6=readImage(45);
+imshow(im6);
+hold on
+
+for d=1:0.01:2
+   
+    pk = Pixel_location([p(1),p(2)],d,P3,P6);
+    plot(pk(1),pk(2),'r.','MarkerSize',20)
+end

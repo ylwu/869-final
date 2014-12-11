be4b58fdab162aaebe02519a66dd73da4382805a
shiyangliu <lsyang@mit.edu>
2014-12-11 00:31:31 -0500

    changed ncc

--- src/Ncc.m
+++ src/Ncc.m
@@ -1,14 +1,9 @@
 function ncc = Ncc(v0,v1)
 %assume these are already in n*3 vector format
 n=size(v0,1);
-new_v0=vertcat(v0(:,1),v0(:,2),v0(:,3));
-new_v1=vertcat(v1(:,1),v1(:,2),v1(:,3));
 %calculate ncc
-v0_mean = sum(new_v0)/(n*3);
-v1_mean = sum(new_v1)/(n*3);
-v0_new = v0-v0_mean;
-v1_new = v1-v1_mean;
-
+v0_new=vertcat(v0(:,1)-mean(v0(:,1)),v0(:,2)-mean(v0(:,2)),v0(:,3)-mean(v0(:,3)));
+v1_new=vertcat(v1(:,1)-mean(v1(:,1)),v1(:,2)-mean(v1(:,2)),v1(:,3)-mean(v1(:,3)));
 
 numerator = sum(dot(v0_new,v1_new,2));
 denom = sqrt(sum(sum(v0_new.*v0_new))+sum(sum(v1_new.*v1_new)));
